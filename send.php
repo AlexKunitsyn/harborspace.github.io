f3ffa3ba635150a9100ee72f57458bb9ae005e11
AlexKunitsyn <58446346+AlexKunitsyn@users.noreply.github.com>
2019-12-15 23:36:14 +0200

    Add files via upload
    ыутв

--- send.php
+++ send.php
@@ -1,17 +1,17 @@
 <?php
-header('Content-type: text/html');
-header('Access-Control-Allow-Origin: *');
-$recepient = 'reyz999@gmail.com'; //TODO не работает отправка на аккаунты gsuite
-$frmname = 'Заявка с сайта harborSpace';
+  header('Content-type: text/html');
+  header('Access-Control-Allow-Origin: *');
+  $recepient = 'reyz999@gmail.com'; //TODO не работает отправка на аккаунты gsuite
+  $frmname = 'Заявка с сайта harborSpace';
+  $phone = $_POST['telephone'];
+  $name = $_POST['nameUser'];
+  $message = "Имя: ".$name . "Телефон: ".$phone;
 
-$message = "Текст Сообщения";
+  if (mail($recepient, $frmname, $message,"From: reyz999@gmail.com\r\n"))
+  {
+      echo "Сообщение успешно отправлено";
+  } else {
+      echo "При отправке сообщения возникли ошибки";
+  }
 
-if (mail($recepient, $frmname, $message,"From: reyz999@gmail.com\r\n"))
-{
-    echo "Сообщение успешно отправлено";
-} else {
-    echo "При отправке сообщения возникли ошибки";
-}
-mail($recepient, $frmname, $message,"From: reyz999@gmail.com\r\n");
-
-?>
+  ?>
